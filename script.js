2c24e50fda8f9d33970e741e83df1d7070887428
Ritesh Girase <142716533+RiteshGirase2003@users.noreply.github.com>
2023-12-27 12:03:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,84 @@
-const wordsList=[
-    "apple", "beach", "table", "chair", "river", "house", "plant", "mouse", "music", "light",
-    "paper", "watch", "stone", "bread", "eagle", "laugh", "radio", "queen", "peace", "heart",
-    "dream", "horse", "green", "smile", "dance", "cloud", "black", "peach", "eagle", "frost",
-    "blaze", "brush", "clean", "crane", "crisp", "drift", "flash", "float", "glaze", "grace",
-    "bliss", "bloom", "charm", "chill", "blend", "blaze", "blaze", "cheer", "clasp", "blend",
-    "bound", "carve", "chase", "charm", "cheer", "climb", "close", "carve", "dance", "dazzl",
-    "dream", "dazzl", "feast", "flame", "flare", "float", "flash", "frost", "fresh", "gaze",
-    "glide", "gloom", "grace", "grand", "green", "gusty", "happy", "heart", "hinge", "humor",
-    "ideal", "ivory", "jewel", "juicy", "jumbo", "junta", "kayak", "kings", "knead", "lance",
-    "larch", "laugh", "leash", "libel", "lucky", "lumpy", "magic", "maple", "medal", "mercy",
-    "mimic", "muddy", "music", "nifty", "noise", "olive", "opera", "optic", "ovary", "panel",
-    "peach", "pedal", "plank", "plain", "plant", "plump", "plush", "prize", "queen", "query",
-    "quick", "quiet", "quite", "quirk", "quite", "quirk", "quirky", "quash", "quell", "quick",
-    "queen", "quirk", "quilt", "quest", "quiet", "quote", "quake", "raven", "relax", "river",
-    "rouge", "rally", "rumba", "radix", "relic", "rouse", "ridge", "round", "royal", "rhino",
-    "savor", "sandy", "shade", "silky", "slice", "slate", "smile", "space", "spade", "stare",
-    "sweet", "swell", "swirl", "taken", "tease", "tense", "thrum", "tidal", "total", "toxic",
-    "tract", "twist", "train", "tally", "unify", "unveil", "ulcer", "under", "unity", "unbox",
-    "untie", "upper", "ultra", "unify"
-  ]
+const wordsList = [
+    "About", "Alert", "Argue", "Beach", "Above", "Alike", "Arise", "Began",
+    "Abuse", "Alive", "Array", "Begin", "Actor", "Allow", "Aside", "Begun",
+    "Acute", "Alone", "Asset", "Being", "Admit", "Along", "Audio", "Below",
+    "Adopt", "Alter", "Audit", "Bench", "Adult", "Among", "Avoid", "Billy",
+    "After", "Anger", "Award", "Birth", "Again", "Angle", "Aware", "Black",
+    "Agent", "Angry", "Badly", "Blame", "Agree", "Apart", "Baker", "Blind",
+    "Ahead", "Apple", "Bases", "Block", "Alarm", "Apply", "Basic", "Blood",
+    "Album", "Arena", "Basis", "Board", "Boost", "Buyer", "China", "Cover",
+    "Booth", "Cable", "Chose", "Craft", "Bound", "Calif", "Civil", "Crash",
+    "Brain", "Carry", "Claim", "Cream", "Brand", "Catch", "Class", "Crime",
+    "Bread", "Cause", "Clean", "Cross", "Break", "Chain", "Clear", "Crowd",
+    "Breed", "Chair", "Click", "Crown", "Brief", "Chart", "Clock", "Curve",
+    "Bring", "Chase", "Close", "Cycle", "Broad", "Cheap", "Coach", "Daily",
+    "Broke", "Check", "Coast", "Dance", "Brown", "Chest", "Could", "Dated",
+    "Build", "Chief", "Count", "Dealt", "Built", "Child", "Court", "Death",
+    "Debut", "Entry", "Forth", "Group", "Delay", "Equal", "Forty", "Grown",
+    "Depth", "Error", "Forum", "Guard", "Doing", "Event", "Found", "Guess",
+    "Doubt", "Every", "Frame", "Guest", "Dozen", "Exact", "Frank", "Guide",
+    "Draft", "Exist", "Fraud", "Happy", "Drama", "Extra", "Fresh", "Harry",
+    "Drawn", "Faith", "Front", "Heart", "Dream", "False", "Fruit", "Heavy",
+    "Dress", "Fault", "Fully", "Hence", "Drill", "Fibre", "Funny", "Night",
+    "Drink", "Field", "Giant", "Horse", "Drive", "Fifth", "Given", "Hotel",
+    "Drove", "Fifty", "Glass", "House", "Dying", "Fight", "Globe", "Human",
+    "Eager", "Final", "Going", "Ideal", "Early", "First", "Grace", "Image",
+    "Earth", "Fixed", "Grade", "Index", "Eight", "Flash", "Grand", "Inner",
+    "Elite", "Fleet", "Grant", "Input", "Empty", "Floor", "Grass", "Issue",
+    "Enemy", "Fluid", "Great", "Irony", "Enjoy", "Focus", "Green", "Juice",
+    "Enter", "Force", "Gross", "Joint", "Judge", "Metal", "Media", "Newly",
+    "Known", "Local", "Might", "Noise", "Label", "Logic", "Minor", "North",
+    "Large", "Loose", "Minus", "Noted", "Laser", "Lower", "Mixed", "Novel",
+    "Later", "Lucky", "Model", "Nurse", "Laugh", "Lunch", "Money", "Occur",
+    "Layer", "Lying", "Month", "Ocean", "Learn", "Magic", "Moral", "Offer",
+    "Lease", "Major", "Motor", "Often", "Least", "Maker", "Mount", "Order",
+    "Leave", "March", "Mouse", "Other", "Legal", "Music", "Mouth", "Ought",
+    "Level", "Match", "Movie", "Paint", "Light", "Mayor", "Needs", "Paper",
+    "Limit", "Meant", "Never", "Party", "Peace", "Power", "Radio", "Round",
+    "Panel", "Press", "Raise", "Route", "Phase", "Price", "Range", "Royal",
+    "Phone", "Pride", "Rapid", "Rural", "Photo", "Prime", "Ratio", "Scale",
+    "Piece", "Print", "Reach", "Scene", "Pilot", "Prior", "Ready", "Scope",
+    "Pitch", "Prize", "Refer", "Score", "Place", "Proof", "Right", "Sense",
+    "Plain", "Proud", "Rival", "Serve", "Plane", "Prove", "River", "Seven",
+    "Plant", "Queen", "Quick", "Shall", "Plate", "Sixth", "Stand", "Shape",
+    "Point", "Quiet", "Roman", "Share", "Pound", "Quite", "Rough", "Sharp",
+    "Sheet", "Spare", "Style", "Times", "Shelf", "Speak", "Sugar", "Tired",
+    "Shell", "Speed", "Suite", "Title", "Shift", "Spend", "Super", "Today",
+    "Shirt", "Spent", "Sweet", "Topic", "Shock", "Split", "Table", "Total",
+    "Shoot", "Spoke", "Taken", "Touch", "Short", "Sport", "Taste", "Tough",
+    "Shown", "Staff", "Taxes", "Tower", "Sight", "Stage", "Teach", "Track",
+    "Since", "Stake", "Teeth", "Trade", "Sixty", "Start", "Texas", "Treat",
+    "Sized", "State", "Thank", "Trend", "Skill", "Steam", "Theft", "Trial",
+    "Sleep", "Steel", "Their", "Tried", "Slide", "Stick", "Theme", "Tries",
+    "Small", "Still", "There", "Truck", "Smart", "Stock", "These", "Truly",
+    "Smile", "Stone", "Thick", "Trust", "Smith", "Stood", "Thing", "Truth",
+    "Smoke", "Store", "Think", "Twice", "Solid", "Storm", "Third", "Under",
+    "Solve", "Story", "Those", "Undue", "Sorry", "Strip", "Three", "Union",
+    "Sound", "Stuck", "Threw", "Unity", "South", "Study", "Throw", "Until",
+    "Space", "Stuff", "Tight", "Upper", "Upset", "Whole", "Waste", "Wound",
+    "Urban", "Whose", "Watch", "Write", "Usage", "Woman", "Water", "Wrong",
+    "Usual", "Train", "Wheel", "Wrote", "Valid", "World", "Where", "Yield",
+    "Value", "Worry", "Which", "Young", "Video", "Worse", "While", "Youth",
+  ];
 
 var selectedWord = '';
 let counter=0;
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
+
 function reload() {
+    const shuffledWords = shuffleArray(wordsList);
     const index = Math.floor(Math.random() * 150);
-    selectedWord = wordsList[index].toUpperCase();
-    // console.log(selectedWord);
+    selectedWord = shuffledWords[index].toUpperCase();
+    console.log(selectedWord);
+
     
     alert(
         "Word Guesser Game\n\n" +
@@ -45,7 +97,6 @@ function reload() {
 
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('load', reload);
-    // document.write("counter : "+counter)
     
     letter_focus();
     buttonClick();
@@ -99,6 +150,10 @@ function buttonClick() {
     });
 }
 
+function convertToSuperscript(count) {
+    const superscriptDigits = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
+    return count.toString().split('').map(digit => superscriptDigits[parseInt(digit)]).join('');
+}
 
 function checkWord(button){
     console.log("\n counter "+counter);
@@ -118,24 +173,37 @@ function checkWord(button){
     let flag= true;
     for (let i = 0; i < inputWord.length; i++) 
     {
-        // console.log(inputWord.length)
+        
         guessedLetter = inputWord[i].toUpperCase();
-        // console.log(guessedLetter);
+        
+
         if (selectedWord.includes(guessedLetter)) 
         {
+            let count = 0;
+            for (let i = 0; i < selectedWord.length; i++) {
+                if (selectedWord[i] === guessedLetter) {
+                    count++;
+                }
+            }
+            const superscriptCount = convertToSuperscript(count);
             
             if (guessedLetter === selectedWord[i]) {
                 inputs[i].style.color= 'green';
                 inputs[i].style.background= 'lightgreen';
-
+                
             }
             else
             {
                 inputs[i].style.color= 'orange';
                 inputs[i].style.background= 'lightyellow';
-
                 flag=false;
             }
+
+            if (count>1)
+            {
+                inputs[i].value =`${guessedLetter}${superscriptCount}`;
+            }
+
         } 
         else {
             inputs[i].style.color= 'red';
@@ -147,7 +215,7 @@ function checkWord(button){
     }
     
     if (flag){
-        alert("Hooray! You Won!");
+        alert("Hooray! You Won! \n\n Attempts : "+(counter+1));
         location.reload();
 
     }
